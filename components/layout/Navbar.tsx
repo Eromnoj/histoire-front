f026aff5e2db710d630f9f82588d5e72630c7bc5
Jonathan Moreschi <j.moreschi@oulook.fr>
2022-12-11 13:38:51 +0100

    testlogout on deploy

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar: FC = () => {
       console.log(res);
       
       dispatch(userSessionLogout())
-      // router.push('/login')
+      router.push('/login')
 
     } catch (error) {
 
