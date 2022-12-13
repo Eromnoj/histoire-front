5ae64b3e8263f6d58f2924ac1ec4f323a016e922
Jonathan Moreschi <j.moreschi@oulook.fr>
2022-12-13 12:29:22 +0100

    image bug

--- next.config.js
+++ next.config.js
@@ -3,18 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname:'localhost',
-        port:'5000',
-        pathname:'/uploads/**'
-      }
       // {
-      //   protocol: 'https',
-      //   hostname: 'calm-earth-06774.herokuapp.com',
-      //   pathname: '/uploads/**'
+      //   protocol: 'http',
+      //   hostname:'localhost',
+      //   port:'5000',
+      //   pathname:'/uploads/**'
       // }
+      {
+        protocol: 'https',
+        hostname: 'calm-earth-06774.herokuapp.com',
+        pathname: '/uploads/**'
+      }
     ]
   }
 }
