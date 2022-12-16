e68f90f52612dc7e3c70b415b45e35898eabf91c
Jonathan Moreschi <j.moreschi@oulook.fr>
2022-12-16 10:20:12 +0100

    test next-cookie 4

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar: FC = () => {
     try {
       const res = await axios(`/api/v1/auth/logout`)
       dispatch(userSessionLogout())
-      setCookie('token', {httpOnly: true, expires: new Date(Date.now())})
+      deleteCookie('token')
       router.push('/login')
 
     } catch (error) {
