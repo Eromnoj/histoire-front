a9f2677d20222dab422abf4ca8faafb5107821f9
Jonathan Moreschi <j.moreschi@oulook.fr>
2022-12-18 09:24:45 +0100

    changind sameSite setting on deleteCookie

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar: FC = () => {
     try {
       const res = await axios(`/api/v1/auth/logout`)
       dispatch(userSessionLogout())
-      deleteCookie('token')
+      deleteCookie('token',{sameSite: 'none'})
       router.push('/login')
 
     } catch (error) {
